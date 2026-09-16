68887c26f9e8624ab5d00b5c1552f14990110cd7
Laís Fernanda Vieira Marinho <rm98824@estudante.fieb.edu.br>
2026-09-16 11:44:31 -0300

    atualizacao do index

--- app/public/scripts/script-index.js
+++ app/public/scripts/script-index.js
@@ -1,1 +1,8 @@
-foto
+const hero = document.querySelector(".hero");
+const heroVideo = document.querySelector(".hero-video");
+
+if (hero && heroVideo) {
+    heroVideo.addEventListener("ended", () => {
+        hero.classList.add("show");
+    });
+}
